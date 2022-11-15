0f058b60ca4bb5bfa6ac6b940c1ee96378776a88
maheshshal <113540170+maheshshal@users.noreply.github.com>
2022-11-15 12:15:30 +0530

    Create child tracker.py

---
+++ DEVELOP THE PYTHON SCRIPT/child tracker.py
@@ -0,0 +1,21 @@
+import json
+import wiotp.sdk.device
+import time
+myconfig = { "idebtity": {
+"orgId": "9v4yiu",
+"typeId": "MyDevice_Mahesh",
+"deviceId": "47272" },
+"auth": {"token": "12345678" }}
+client = wiotp.sdk.device.Deviceclient(config=myconfig,logHandlers=None)
+client.connect()
+while True:
+    name= "Smartbridge" #in area location
+    #latitude=17.4225176 #longitude=78.5458842
+    #out area location
+    latitude=17.4219272
+    longitude=78.5488783
+    myData={'name': name, 'lat': latitude,'lon': longitude}
+    client.publishEvent(eventId="status",msgformat="json", data=mydata, qos=0, onpublish=None)
+    print("Data published to IBM IOT platform :",myData)
+    time.sleep(5)
+    client.disconnect();
